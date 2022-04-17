8873c62d285dbe96733b2f8c686e6069dae49844
Jay-flow <kimpetro153@gmail.com>
2022-04-17 13:20:23 +0900

    Create the disabled tab bar

---
+++ lib/src/disabled_tab_bar.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class DisabledTabBar extends StatefulWidget {
+  const DisabledTabBar({
+    Key? key,
+    required this.tabs,
+    this.controller,
+    this.isScrollable = false,
+    this.padding,
+    this.indicatorColor,
+    this.automaticIndicatorColorAdjustment = true,
+    this.indicatorWeight = 2.0,
+    this.indicatorPadding = EdgeInsets.zero,
+    this.indicator,
+    this.indicatorSize,
+    this.labelColor,
+    this.labelStyle,
+    this.labelPadding,
+    this.unselectedLabelColor,
+    this.unselectedLabelStyle,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.overlayColor,
+    this.mouseCursor,
+    this.enableFeedback,
+    this.onTap,
+    this.physics,
+  }) : super(key: key);
+
+  final List<Widget> tabs;
+  final TabController? controller;
+  final bool isScrollable;
+  final EdgeInsetsGeometry? padding;
+  final Color? indicatorColor;
+  final bool automaticIndicatorColorAdjustment;
+  final double indicatorWeight;
+  final EdgeInsetsGeometry indicatorPadding;
+  final Decoration? indicator;
+  final TabBarIndicatorSize? indicatorSize;
+  final Color? labelColor;
+  final TextStyle? labelStyle;
+  final EdgeInsetsGeometry? labelPadding;
+  final Color? unselectedLabelColor;
+  final TextStyle? unselectedLabelStyle;
+  final DragStartBehavior dragStartBehavior;
+  final MaterialStateProperty<Color?>? overlayColor;
+  final MouseCursor? mouseCursor;
+  final bool? enableFeedback;
+  final void Function(int)? onTap;
+  final ScrollPhysics? physics;
+
+  @override
+  State<DisabledTabBar> createState() => _DisabledTabBarState();
+}
+
+class _DisabledTabBarState extends State<DisabledTabBar> {
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      tabs: widget.tabs,
+      controller: widget.controller,
+      isScrollable: widget.isScrollable,
+      padding: widget.padding,
+      indicatorColor: widget.indicatorColor,
+      automaticIndicatorColorAdjustment:
+          widget.automaticIndicatorColorAdjustment,
+      indicatorWeight: widget.indicatorWeight,
+      indicatorPadding: widget.indicatorPadding,
+      indicator: widget.indicator,
+      indicatorSize: widget.indicatorSize,
+      labelColor: widget.labelColor,
+      labelStyle: widget.labelStyle,
+      labelPadding: widget.labelPadding,
+      unselectedLabelColor: widget.unselectedLabelColor,
+      unselectedLabelStyle: widget.unselectedLabelStyle,
+      dragStartBehavior: widget.dragStartBehavior,
+      overlayColor: widget.overlayColor,
+      mouseCursor: widget.mouseCursor,
+      enableFeedback: widget.enableFeedback,
+      onTap: widget.onTap,
+      physics: widget.physics,
+    );
+  }
+}
